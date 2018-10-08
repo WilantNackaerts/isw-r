b76f133523875c93bc3ea8e6bbcb91f1ad07a2b8
Tuur Dutoit <me@tuurdutoit.be>
2018-10-08 16:47:16 +0200

    Make React devtools work

--- app/store/store.js
+++ app/store/store.js
@@ -2,12 +2,17 @@
 
 import { createStore } from 'redux';
 
-export default createStore( function( state = { test: 'test' }, action ) {
+const reactDevtoolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__();
+
+function reducer( state = { test: 'test' }, action ) {
   switch ( action.type ) {
     case 'setTest':
       return { ...state, test: action.test };
     default:
       return state;
-      
+
   }
-} ); 
+}
+
+export default createStore( reducer, reactDevtoolsExtension ); 
