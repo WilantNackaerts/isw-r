4058cfe00f3e64e21c506ef880719ba99d42b6ea
WilantNackaerts <wilantn@hotmail.com>
2018-10-10 10:57:14 +0200

    added types

--- app/store/actions/media/soundboard.js
+++ app/store/actions/media/soundboard.js
@@ -1,6 +1,12 @@
 // @flow
 import { SOUNDBOARD_SETSOUNDS } from '../../actions';
 
+type SoundboardSetSoundAction = {
+  type: typeof SOUNDBOARD_SETSOUND,
+  sounds: [],
+  folders: []
+}
+
 export function soundboardSetSounds( sounds, folders ): SoundboardSetSoundAction {
   return {
     type: SOUNDBOARD_SETSOUNDS,
