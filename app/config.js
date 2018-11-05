1ecfd66fb1d91acc7f2c3a3899a30c13cc33fc33
Wilant Nackaerts <wilant@pop-os.localdomain>
2018-11-05 11:49:42 +0100

    youtube calls working

--- app/config.js
+++ app/config.js
@@ -6,4 +6,5 @@ export const DEVELOPMENT = NODE_ENV === 'development';
 export const MEDIA_URL = 'http://m.isw';
 export const MEDIA_API_URL = MEDIA_URL + '/api';
 export const SOUNDBOARD_URL = MEDIA_API_URL + '/sb';
+export const YOUTUBE_API_URL = MEDIA_API_URL + '/yt/byid'; 
 export const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search?type=video&maxResults=10&part=snippet&key='+ YOUTUBE_API_KEY +'&q=';
