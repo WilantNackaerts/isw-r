d5b4fd5abd6c76870b5d02d1625c2e3cadaf2b5c
Wilant Nackaerts <wilant.nackaerts@gmail.com>
2018-11-20 16:26:30 +0100

    Order screen

--- app/screens/shop/Order.js
+++ app/screens/shop/Order.js
@@ -1,12 +1,58 @@
 // @flow
 
 import React, { Component } from 'react';
-import { Text } from 'native-base';
+import { connect } from 'react-redux';
+import { Text, Container, List, ListItem, Button, Footer, Left, Right } from 'native-base';
+import type { NavigationScreenProp } from 'react-navigation';
+import type { State, Dispatch } from '/types';
+import type { Product, Basket } from '/types/shop';
 
-export default class Order extends Component<*> {
+type Props = {
+  loading: boolean,
+  navigation: NavigationScreenProp,
+  productsById: Product[],
+  basket: Basket,
+  total: number,
+}
+
+class Order extends Component<Props> {
   render() {
     return (
-      <Text>Order</Text>
+      <Container>
+        <List dataArray={Object.keys( this.props.basket )} 
+          renderRow={( id ) =>
+            <ListItem>
+              <Text>{this.props.productsById[ id ].name}</Text>
+              <Text>{this.props.productsById[ id ].price * this.props.basket[ id ]}</Text>
+            </ListItem>
+          } />
+        <Footer>
+          <Left>
+            <Button rounded success>
+              <Text>Pay</Text>
+            </Button>
+          </Left>
+          <Right>
+            <Text>{this.props.total}</Text>
+          </Right>
+        </Footer>
+      </Container>
     );
   }
 }
+
+function mapStateToProps( state: State ) {
+  return {
+    basket: state.shop.basket,
+    productsById: state.shop.productsById,
+    total: state.shop.total,
+  };
+}
+
+function mapDispatchToProps( dispatch: Dispatch ) {
+  return {
+    
+  };
+}
+
+export default connect( mapStateToProps, mapDispatchToProps )( Order );
