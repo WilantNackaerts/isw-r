98dd9fdc7be3b0b80aa8adf3ac36c10acebb6f13
Wilant Nackaerts <wilant.nackaerts@gmail.com>
2018-11-28 14:23:51 +0100

    updated order screen

--- app/screens/shop/Order.js
+++ app/screens/shop/Order.js
@@ -2,6 +2,7 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { StyleSheet } from 'react-native';
 import { Text, Container, List, ListItem, Button, Footer, Left, Right } from 'native-base';
 import type { NavigationScreenProp } from 'react-navigation';
 import type { State, Dispatch } from '/types';
@@ -22,24 +23,35 @@ class Order extends Component<Props> {
         <List dataArray={Object.keys( this.props.basket )} 
           renderRow={( id ) =>
             <ListItem>
-              <Text>{this.props.productsById[ id ].name}</Text>
-              <Text>{this.props.productsById[ id ].price * this.props.basket[ id ]}</Text>
+              <Left>
+                <Text>{this.props.productsById[ id ].name}: </Text>
+                <Text>€ {this.props.productsById[ id ].price * this.props.basket[ id ]} </Text>
+              </Left>
+              <Right>
+                <Text>amount: {this.props.basket[ id ]}</Text>
+              </Right>
             </ListItem>
           } />
         <Footer>
           <Left>
-            <Button rounded success>
+            <Button rounded success style={styles.payButton}>
               <Text>Pay</Text>
             </Button>
           </Left>
           <Right>
-            <Text>{this.props.total}</Text>
+            <Text>€ {this.props.total}</Text>
           </Right>
         </Footer>
       </Container>
     );
   }
 }
+
+const styles = StyleSheet.create( {
+  payButton: {
+    marginLeft: 20,
+  },
+} );
 
 function mapStateToProps( state: State ) {
   return {
