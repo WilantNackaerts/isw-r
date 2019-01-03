1192601c623ca54ea318c587184a37c6cde16527
Wilant Nackaerts <wilant.nackaerts@gmail.com>
2019-01-03 13:29:19 +0100

    cams almost done

--- app/config.js
+++ app/config.js
@@ -15,3 +15,5 @@ export const MEDIA_STATUS_URL = MEDIA_API_URL + '/status';
 export const SHOP_URL = 'http://10.0.2.2:3050/shop';
 export const SHOP_API_URL = SHOP_URL + '/api/v1';
 export const SHOP_API_ORDER_URL = SHOP_API_URL + '/user/orders';
+
+export const CAMS_URL = 'https://cams.iswleuven.be/ISW';
--- app/screens/Cams.js
+++ app/screens/Cams.js
@@ -1,12 +1,83 @@
 // @flow
 
 import React, { Component } from 'react';
-import { Text } from 'native-base';
+import { Image, StyleSheet, Dimensions } from 'react-native';
+import { Container, Content, List, ListItem } from 'native-base';
+import type { NavigationScreenProp } from 'react-navigation';
+import { CAMS_URL } from '/config';
+import { CAMS_LOGIN } from '/secret';
 
-export default class Cams extends Component<{}> {
+type Props = {
+  navigation: NavigationScreenProp,
+}
+
+type State = {
+  cams: string[]
+}
+
+export default class Cams extends Component<Props, State> {
+  interval: ?IntervalID;
+  state = {
+    cams: [],
+  }
+
+  componentDidMount() {
+    this.startPoll();
+    this.props.navigation.addListener( 'didFocus', this.startPoll.bind( this ) );
+    this.props.navigation.addListener( 'willBlur', this.stopPoll.bind( this ) );
+  }
+
+  startPoll() {
+    if ( !this.interval ) {
+      this.interval = setInterval( this.getCams.bind( this ), 2000 );
+      this.getCams();
+    }
+  }
+
+  stopPoll() {
+    if ( this.interval ) {
+      clearInterval( this.interval );
+      this.interval = null;
+    }
+  }
+
+
+  getCams() {
+    let cams = [];
+    for ( let i = 0; i < 4; i++ ) {
+      cams.push( CAMS_URL + i + '?rand=' + Math.random() );
+      console.log( cams[ i ] );
+    }
+    this.setState( { cams } );
+  }
+
   render() {
     return (
-      <Text>Cams</Text>
+      <Container>
+        <Content>
+          <List dataArray={this.state.cams}
+            renderRow={( cam ) =>
+              <ListItem>
+                <Image style={styles.image} fadeDuration={0} source={{ 
+                  uri: cam,
+                  headers: {
+                    Authorization: 'Basic ' + CAMS_LOGIN,
+                  },
+                }}
+                />
+              </ListItem>
+            } />
+        </Content>
+      </Container>
     );
   }
 }
+
+let width = Dimensions.get( 'window' ).width;
+
+const styles = StyleSheet.create( {
+  image: {
+    width: width - 35,
+    height: 400,
+  },
+} );
