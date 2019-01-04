ba1784eb09a2a1b0a570b137edd50784b441d0d7
Wilant Nackaerts <wilant.nackaerts@gmail.com>
2019-01-04 15:15:45 +0100

    splashscreen

--- android/app/src/main/java/com/r/MainActivity.java
+++ android/app/src/main/java/com/r/MainActivity.java
@@ -1,6 +1,7 @@
 package com.r;
-
+import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +12,10 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "r";
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
     }
 }
